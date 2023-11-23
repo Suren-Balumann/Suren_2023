3d5a1045645d906840de79998e3cd82dfec5cbc9
surentumbashyan <u.s.b-97@mail.ru>
2023-11-23 09:06:19 +0300

    start_file

--- suren_2023/suren_2023/settings.py
+++ suren_2023/suren_2023/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'project',
+    'hello_world'
 ]
 
 MIDDLEWARE = [
@@ -54,7 +56,7 @@ ROOT_URLCONF = 'suren_2023.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': ['Template'],
+        'DIRS': ['suren_2023/Template'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
--- suren_2023/suren_2023/urls.py
+++ suren_2023/suren_2023/urls.py
@@ -15,10 +15,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from . import vews
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', vews.home)
+    path('', include('hello_world.urls')),
+    path('', include('project.urls'))
 ]
